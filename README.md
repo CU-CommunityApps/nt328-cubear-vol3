24200233269e607261a6e36b9631069fd36a7a44
Nick T <sbbutkcin@gmail.com>
2017-09-22 09:17:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# nt328-cubear-vol3
+
+![CI none](https://img.shields.io/badge/ci-none-orange.svg)
+[![Dashboard nt328-cubear-vol3](https://img.shields.io/badge/dashboard-nt328_cubear_vol3-yellow.svg)](https://dashboard.pantheon.io/sites/dbaead89-b8d2-4e72-986b-f611d25b6295#dev/code)
+[![Dev Site nt328-cubear-vol3](https://img.shields.io/badge/site-nt328_cubear_vol3-blue.svg)](http://dev-nt328-cubear-vol3.pantheonsite.io/)
---
+++ web/sites/default/settings.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all envrionments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to ensure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . "/settings.pantheon.php";
+
+/**
+ * Place the config directory outside of the Drupal root.
+ */
+$config_directories = array(
+  CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
+);
+
+/**
+ * Load local development override configuration, if available.
+ */
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
